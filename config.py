d03e1ae7185c49b7c8d7b3c8027b167e2f304b4b
samarbhamra <samardeepbhamra@gmail.com>
2024-09-07 18:05:37 -0400

    first commit

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+DATABASE_CONFIG = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': 'your_root_password',  # Replace with your MySQL password
+    'database': 'fitness_data'
+}
+
