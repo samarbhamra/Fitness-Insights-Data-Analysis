b1096f4c65da5ed86690126e3ec737462d6c52c1
samarbhamra <samardeepbhamra@gmail.com>
2024-09-08 00:15:46 -0400

    l

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # config.py
 DATABASE_CONFIG = {
-    'host': '127.0.0.1',  # localhost or the IP address where DBngin runs the MySQL service
-    'port': 3306,  # or the port DBngin is using (default is 3306)
-    'user': 'root',  # or the username you set in DBngin
-    'password': 'your_mysql_password',  # the password you set in DBngin
-    'database': 'fitness_data'  # your database name
+    'host': '12.4',  # localhost
+    'port': 3306,  # DBngin MySQL port (default is 3306)
+    'user': 'root',  # Default MySQL user
+    'password': '',  # No password, so leave this as an empty string
+    'database': 'fitness_data'  # Name of the database you're using
 }
